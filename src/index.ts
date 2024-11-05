95e4a6fe9a7e3f2cc98985cd32a11188fc25a4b9
leumasic <53974949+leumasic@users.noreply.github.com>
2024-11-05 20:14:13 +0000

    enhancements

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,33 @@
 // Graciously taken from https://github.com/sindresorhus/type-fest/tree/main
-type ArrayTail<T> = T extends readonly [unknown, ...infer Tail] ? Tail : [];
+type ArrayTail<T> = T extends readonly [unknown, ...infer Tail] ? Tail : []
 
-export type KeyValuePair<K, V> = {
-  key: K
-  value: V
-}
+type KeyValuePair<K, V> = [K, V]
 type KeyValuePairList<K = any, V = any> = KeyValuePair<K, V>[]
 
-type ExtractKeys<T extends KeyValuePairList> = T[number]['key']
-type ExtractValues<T extends KeyValuePairList, K> = T[0] extends KeyValuePair<K, infer Value> ? Value : T[0] extends undefined ? never : ExtractValues<ArrayTail<T>, K>
+type ExtractKeys<T extends KeyValuePairList> = T[number][0]
+type ExtractValues<T extends KeyValuePairList> = T[number][1]
+
+type ExtractValueOfKey<T extends KeyValuePairList, K> = T[0] extends KeyValuePair<K, infer Value>
+  ? Value
+  : T[0] extends undefined
+    ? never
+    : ExtractValueOfKey<ArrayTail<T>, K>
 
 export class ConstrainedMap<T extends KeyValuePairList> extends Map<
   ExtractKeys<T>,
-  ExtractValues<T, ExtractKeys<T>>
+  ExtractValues<T>
 > {
-  get<K extends ExtractKeys<T>>(key: K): ExtractValues<T, K> | undefined {
-    return super.get(key) as ExtractValues<T, K> | undefined
+  constructor(elements?: T)
+  constructor(elements?: T[number][])
+  constructor(elements?: Iterable<readonly [ExtractKeys<T>, ExtractValues<T>]> | null) {
+    super(elements)
   }
 
-  set<K extends ExtractKeys<T>>(key: K, value: ExtractValues<T, K>): this {
+  get<K extends ExtractKeys<T>>(key: K): ExtractValueOfKey<T, K> | undefined {
+    return super.get(key) as ExtractValueOfKey<T, K> | undefined
+  }
+
+  set<K extends ExtractKeys<T>>(key: K, value: ExtractValueOfKey<T, K>): this {
     return super.set(key, value)
   }
 }
